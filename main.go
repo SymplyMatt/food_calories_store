d54c52f214d3ed2dda96a02a730ba6ef37cbbf87
Matt <symplymatt@gmail.com>
2024-01-05 02:34:04 +0100

    removed comments

--- main.go
+++ main.go
@@ -22,7 +22,6 @@ func main() {
 	router.GET("/entries", routes.GetEntries)
 	router.GET("/entry/:id/", routes.GetEntryById)
 	router.GET("/ingredient/:ingredient", routes.GetEntriesByIngredient)
-
 	router.PUT("/entry/update/:id", routes.UpdateEntry)
 	router.PUT("/ingredient/update/:id", routes.UpdateIngredient)
 	router.DELETE("/entry/delete/:id", routes.DeleteEntry)
